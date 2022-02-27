dc69b8eab317e8646cc1f455648c952431512474
unknown <nawfal.phoenix@gmail.com>
2022-02-27 13:49:57 +0100

    added some challenges

---
+++ starter/script.js
@@ -0,0 +1,56 @@
+"use strict";
+/////////////////////////////////////////////////////
+//////// -- JS Fundamentals part 2 -- //////////////
+////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////
+////////////// -- Challenge #1 -- //////////////////
+////////////////////////////////////////////////////
+/*
+const calcAverage = (score1, score2, score3) => {
+  const avgScore = (score1 + score2 + score3) / 3;
+  return avgScore;
+};
+
+const avgDolphins = calcAverage(44, 23, 71);
+const avgKoalas = calcAverage(65, 54, 49);
+
+const checkWinner = function (avgDolphins, avgKoalas) {
+  if (avgDolphins > avgKoalas && avgDolphins >= 2 * avgKoalas) {
+    console.log(`Dolphins win with ${avgDolphins} points`);
+  } else if (avgKoalas > avgDolphins && avgKoalas >= 2 * avgDolphins) {
+    console.log(`Koalass win with ${avgKoalas} points`);
+  } else {
+    console.log(`No one wins`);
+  }
+};
+
+checkWinner(avgDolphins, avgKoalas);
+*/
+
+////////////////////////////////////////////////////
+////////////// -- Challenge #2 -- //////////////////
+////////////////////////////////////////////////////
+
+/*
+const calcTip = (bill) => {
+  let tip;
+  if (bill <= 300 && bill >= 50) {
+    tip = bill * 0.15;
+  } else {
+    tip = bill * 0.2;
+  }
+  return tip;
+};
+
+console.log(calcTip(400));
+
+const tips = [calcTip(125), calcTip(555), calcTip(44)];
+console.log(tips);
+const total = [tips[0] + 125, tips[1] + 555, tips[2] + 44];
+console.log(total);
+*/
+
+////////////////////////////////////////////////////
+////////////// -- Challenge #2 -- //////////////////
+////////////////////////////////////////////////////
