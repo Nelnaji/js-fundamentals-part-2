fa96935a8ce883d3f03cfacbf729dded02be1622
unknown <nawfal.phoenix@gmail.com>
2022-03-02 16:43:24 +0100

    this is the end

--- starter/script.js
+++ starter/script.js
@@ -52,5 +52,76 @@ console.log(total);
 */
 
 ////////////////////////////////////////////////////
-////////////// -- Challenge #2 -- //////////////////
+////////////// -- Challenge #3 -- //////////////////
 ////////////////////////////////////////////////////
+/*
+const mark = {
+  firstName: "Mark",
+  lastName: "Miller",
+  age: 44,
+  mass: 78,
+  height: 1.69,
+  calcBMI: function () {
+    this.bmi = this.mass / this.height ** 2;
+    return this.bmi;
+  },
+};
+
+const john = {
+  firstName: "John",
+  lastName: "Smith",
+  age: 38,
+  mass: 92,
+  height: 1.95,
+  calcBMI: function () {
+    this.bmi = this.mass / this.height ** 2;
+    return this.bmi;
+  },
+};
+
+// you must call the method because it does not call itself
+
+mark.calcBMI();
+john.calcBMI();
+
+if (mark.bmi > john.bmi) {
+  console.log("Mark is heavier");
+} else {
+  console.log("John is Heavier");
+}
+*/
+
+////////////////////////////////////////////////////
+////////////// -- Challenge #4 -- //////////////////
+////////////////////////////////////////////////////
+/*
+const bill = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+
+const calcTip = (bill) => {
+  let tip;
+
+  if (bill <= 300 && bill >= 50) {
+    tip = bill * 0.15;
+  } else {
+    tip = bill * 0.2;
+  }
+  let total = tip + bill;
+  return total;
+};
+
+const calcAverage = function (parametre) {
+  let sum = 0;
+  for (let i = 0; i <= parametre.length - 1; i++) {
+    sum = sum + parametre[i];
+  }
+  let average = sum / parametre.length;
+  console.log(average);
+  return average;
+};
+
+for (let i = 0; i < bill.length - 1; i++) {
+  console.log(calcTip(bill[i]));
+}
+
+calcAverage(bill);
+*/
